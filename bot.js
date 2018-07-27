9d1ab4b857fbc12338663feb655839ab8bc60ae5
Kelvin Chua <chuakid@gmail.com>
2018-07-28 01:13:41 +0800

    added d20

--- bot.js
+++ bot.js
@@ -14,25 +14,33 @@ var bot = new Discord.Client({
 });
 
 
-bot.on('ready', function(evt) {
+bot.on('ready', function (evt) {
   logger.info('Connected');
   logger.info('Logged in as: ');
   logger.info(bot.username + ' - (' + bot.id + ')');
 });
 
+
 bot.on('message', function (user, userID, channelID, message, evt) {
-  if(message.substring(0,1) == '!') {
+  if (message.substring(0, 1) == '!') {
     var args = message.substring(1).split(' ');
     var command = args[0];
 
     args = args.splice(1);
-    switch(command) {
+    switch (command) {
       case 'ping':
-      bot.sendMessage({
-        to: channelID,
-        message: 'Pong!'
-      });
-      break;
+        bot.sendMessage({
+          to: channelID,
+          message: 'Pong!'
+        });
+        break;
+      case 'd20':
+        let a = Math.floor(Math.random() * 19 + 1)
+        bot.sendMessage({
+          to: channelID,
+          message: a
+        })
+        break;
     }
   }
 });
